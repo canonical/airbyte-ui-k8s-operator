d38879865d6c9ccd5bb8d731eae8b25846f2041d
Ali Kelkawi <ali.kelkawi@canonical.com>
2024-10-16 12:21:07 +0300

    rename image to airbyte-ui

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,4 +15,4 @@ def pytest_addoption(parser: pytest.Parser):
     # The prebuilt charm file.
     parser.addoption("--charm-file", action="append", default=[])
     # The charm image name:tag.
-    parser.addoption("--airbyte-webapp-image", action="store", default="")
+    parser.addoption("--airbyte-ui-image", action="store", default="")
